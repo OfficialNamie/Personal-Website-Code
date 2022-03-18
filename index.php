b3183c8220df6f3f3c0c86ab26a64d319a06618d
Namie <84647883+OfficialNamie@users.noreply.github.com>
2022-03-18 17:08:42 +0100

    index.php
    Here is the main file.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <script src="https://use.fontawesome.com/d1341f9b7a.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="your-icon.png">
+    <title>Namie Developing</title>
+    <script src="https://kit.fontawesome.com/e87e1c4f35.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="box">
+          <img src="your-profile.png" alt="" class="box-img">
+          <h1>Your Name Here</h1>
+          <h5>Short about you</h5>
+          <p>Long about you.</p>
+      <ul>
+          <li><a href="#"><i class="fa-brands fa-discord" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-youtube" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-github" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-twitter" aria-hidden="true"></i></a></li>
+      </ul>
+    </div>
+  </body>
+</html>
+
